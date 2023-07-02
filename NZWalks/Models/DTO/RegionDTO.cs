f2eeaa47b663fc46f0b51c7f85505be7557c7ea1
yannakode <yanann1997@gmail.com>
2023-07-02 00:15:10 -0300

    Model validations

--- NZWalks/Models/DTO/RegionDTO.cs
+++ NZWalks/Models/DTO/RegionDTO.cs
@@ -1,9 +1,17 @@
-﻿namespace NZWalks.Models.Domain.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.Models.Domain.DTO
 {
     public class RegionDTO
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The code is required")]
+        [StringLength(3, ErrorMessage = "The code must have only 3 characters")]
         public string Code { get; set; }
+
+        [Required(ErrorMessage = "The name is required")]
+        [StringLength(100, ErrorMessage = "The name must have 100 characters or fewert")]
         public string Name { get; set; }
         public string? RegionImageURL { get; set; }
     }
--- NZWalks/Models/DTO/WalkDTO.cs
+++ NZWalks/Models/DTO/WalkDTO.cs
@@ -1,13 +1,22 @@
 ﻿using NZWalks.Models.Domain;
 using NZWalks.Models.Domain.DTO;
+using System.ComponentModel.DataAnnotations;
 
 namespace NZWalks.Models.DTO
 {
     public class WalkDTO
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The name is required")]
+        [StringLength(100, ErrorMessage = "The name must have 100 characters or fewer")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "The description is required")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "The length must be a positive value")]
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
 
